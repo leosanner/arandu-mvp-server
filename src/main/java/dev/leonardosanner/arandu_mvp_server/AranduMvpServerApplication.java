33d859de931f3418e18395ae0c917b06687ec092
leosanner <leosanner12@gmail.com>
2025-07-20 13:35:54 -0300

    feat: first commit

---
+++ src/main/java/dev/leonardosanner/arandu_mvp_server/AranduMvpServerApplication.java
@@ -0,0 +1,13 @@
+package dev.leonardosanner.arandu_mvp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AranduMvpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AranduMvpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leonardosanner/arandu_mvp_server/AranduMvpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonardosanner.arandu_mvp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AranduMvpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
